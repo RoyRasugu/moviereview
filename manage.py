3926d65e438c01efb6791c0d50ef2116372e6ec0
Roy Rasugu <roy.rasugu@student.moringaschool.com>
2021-09-16 14:59:11 +0300

    new deployment

--- manage.py
+++ manage.py
@@ -1,5 +1,5 @@
 # import re
-from app import create_app,db
+from app import create_app
 from flask_script import Manager,Server
 from app.models import User
 
@@ -14,10 +14,6 @@ def test():
     import unittest
     tests = unittest.TestLoader().discover('tests')
     unittest.TextTestRunner(verbosity=2).run(tests)
-
-@manager.shell
-def make_shel_context():
-    return dict(app = app,db = db, User = User )
    
 if __name__ == '__main__':
     manager.run()
